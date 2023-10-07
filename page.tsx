bbf2929be9080661e8656f050692d4d392da8be4
JasonGaid <143233399+JasonGaid@users.noreply.github.com>
2023-10-07 10:16:39 +0800

    Add files via upload

---
+++ page.tsx
@@ -0,0 +1,23 @@
+"use client"
+import { useState } from 'react';
+
+export default function Counter() {
+  const [count, setCount] = useState(0);
+
+  const increment = () => {
+    setCount(count + 1);
+  };
+
+  const decrement = () => {
+    setCount(count - 1);
+  };
+
+  return (
+    <div className='center'>
+      <h1>Counter App</h1>
+      <p>Count: {count}</p>
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
+    </div>
+  );
+}
